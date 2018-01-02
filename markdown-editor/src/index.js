305bb2cdc0469f25aea246a635707a1116bb560d
ajayns <ajay.ns@outlook.com>
2018-01-02 19:13:16 +0530

    Add markdown-editor

---
+++ markdown-editor/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Remarkable from 'remarkable';    
+
+class MarkdownEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { 
+            value: 'Type some *markdown* here!'
+        };
+    }
+
+
+    handleChange(e) {
+        this.setState({ 
+            value: e.target.value
+        });
+    }
+
+    getRawMarkup() {
+        const md = new Remarkable();
+        return {__html: md.render(this.state.value)};
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Input</h3>
+                <textarea
+                    onChange={this.handleChange}
+                    defaultValue={this.state.value}
+                />
+                <br />
+                <h3>Output</h3>
+                <div dangerouslySetInnerHTML={this.getRawMarkup()}></div>
+            </div>
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <MarkdownEditor />,
+    document.getElementById('root')
+);
