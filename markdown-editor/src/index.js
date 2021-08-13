fe5ab2bd3affc541dfd8596e2f02a50a34c917fe
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-08-13 12:10:25 +0200

    rewrite markdown editor to functional components

--- markdown-editor/src/index.js
+++ markdown-editor/src/index.js
@@ -1,56 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Remarkable from 'remarkable';    
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Remarkable from "remarkable";
 
-class MarkdownEditor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.state = { 
-            value: 'Type some *markdown* here!'
-        };
-    }
+const MarkdownEditor = () => {
+  const [text, setText] = React.useState("Type some *markdown* here!");
 
+  const getRawMarkup = () => {
+    const md = new Remarkable();
+    return { __html: md.render(text) };
+  };
 
-    handleChange(e) {
-        this.setState({ 
-            value: e.target.value
-        });
-    }
-
-    getRawMarkup() {
-        const md = new Remarkable();
-        return {__html: md.render(this.state.value)};
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="input">
-                    <h3>Input</h3>
-                <textarea
-                    className="input-text"
-                    onChange={this.handleChange}
-                    defaultValue={this.state.value}
-                />
-                </div>
-                <div className="output">
-                    <h3>Markdown</h3>
-                <div 
-                    dangerouslySetInnerHTML={this.getRawMarkup()}
-                    className="output-text"
-                >
-                </div>
-                </div>                
-            </div>
-        )
-    }
-}
+  return (
+    <div className="container">
+      <div className="input">
+        <h3>Input</h3>
+        <textarea
+          className="input-text"
+          onChange={e => setText(e.target.value)}
+          defaultValue={text}
+        />
+      </div>
+      <div className="output">
+        <h3>Markdown</h3>
+        <div
+          dangerouslySetInnerHTML={getRawMarkup()}
+          className="output-text"
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 // ========================================
 
-ReactDOM.render(
-    <MarkdownEditor />,
-    document.getElementById('root')
-);
+ReactDOM.render(<MarkdownEditor />, document.getElementById("root"));
